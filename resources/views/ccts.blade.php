f31efe584422ef9d086849edb1f9e3cba1736c98
SergioR27 <checo27.aguilar@gmail.com>
2026-03-17 17:32:06 -0600

    Cambio en page cct

--- resources/views/ccts.blade.php
+++ resources/views/ccts.blade.php
@@ -27,7 +27,7 @@
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-5 mt-5">
                                 <div>
                                     <label for="nombre_plantel"
-                                        class="block mb-2 text-xs font-medium text-gray-600 dark:text-white">Nombre
+                                        class="block mb-2 text-xs font-medium text-gray-600 dark:text-white">Nombre del
                                         Plantel</label>
                                     <input type="text"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-vino focus:border-vino block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-vino dark:focus:border-vino"
